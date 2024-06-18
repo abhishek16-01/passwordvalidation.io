2ab3ce114092b8f9b3e18fd0cef6d405f580a222
unknown <raynade.abhishek@gmail.com>
2024-06-18 11:06:14 +0530

    src

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React, { useState } from "react"; 
+import validator from 'validator'
+
+const App = () => { 
+
+	const [errorMessage, setErrorMessage] = useState('') 
+
+	const validate = (value) => { 
+
+		if (validator.isStrongPassword(value, { 
+			minLength: 8, minLowercase: 1, 
+			minUppercase: 1, minNumbers: 1, minSymbols: 1 
+		})) { 
+			setErrorMessage('Is Strong Password') 
+		} else { 
+			setErrorMessage('Is Not Strong Password') 
+		} 
+	} 
+
+	return ( 
+		<div style={{ 
+			marginLeft: '450px', 
+			
+		}}> 
+
+		
+			<pre> 
+				<h1>Password Validation </h1>
+				<h1>Checking Password Strength in ReactJS</h1> 
+				<h2>Enter Password: </h2><input type="text" 
+					onChange={(e) => validate(e.target.value)}></input> <br /> 
+				{errorMessage === '' ? null : 
+					<span style={{ 
+						fontWeight: 'bold', 
+						color: 'red', 
+					}}>{errorMessage}</span>} 
+			</pre> 
+			
+			<h4>Note:<br></br>1. Password should have at least one numerical digit(0-9). 
+			<br></br> 2. Password's length should be in between 8 to 15 characters.
+			<br></br>3. Password should have at least one lowercase letter(a-z).
+			<br></br>4. Password should have at least one uppercase letter(A-Z). 
+			<br></br>5. Password should have at least one special character ( @, #, %, &, !, $, *).</h4>
+			
+		</div> 
+	); 
+} 
+
+export default App
